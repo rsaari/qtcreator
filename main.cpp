0b0ea1c420dbc9a9900a47b8e19032f142fa134f
rsaari <rsaari@ovi.com>
2014-01-08 20:47:22 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <QtGui/QApplication>
+#include <QtDeclarative/QDeclarativeContext>
 #include "qmlapplicationviewer.h"
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+    QString myButtonText = "C++ button";
+    QString myLabelText = "Hello, QML world!";
+
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
     QmlApplicationViewer viewer;
+    viewer.rootContext()->setContextProperty("myButton",myButtonText);
+    viewer.rootContext()->setContextProperty("myLabel",myLabelText);
+
     viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
     viewer.setMainQmlFile(QLatin1String("qml/ContextProperty4/main.qml"));
     viewer.showExpanded();
